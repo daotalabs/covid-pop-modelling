1e9d7c6e4c98cc9d349adc57c24c970c6bd9a52b
Viet Dao <vietdao204@gmail.com>
2022-10-18 10:07:51 -0700

    better doc

--- R/make_history.R
+++ R/make_history.R
@@ -3,27 +3,37 @@
 #' @param N population size
 #' @param M augmented population size
 #' @param k number of capture occasions
-#' @param pa probability of being available for a lab test
-#' @param pb probability of being available for hospitalization
-#' @param theta1 capture probability of lab test
-#' @param theta2 capture probability of hospitalization
+#' @param pa capture probability of lab test
+#' @param pb capture probability of lab test hospitalization
+#' @param theta1 probability of being available for lab test
+#' @param theta2 probability of being available for hospitalization
 #'
-#' @return list of capture and availability histories
-#' @export
+#' @other `a` latent indicator of whether individual is available for capture at lab test
+#' @other `b` latent indicator of whether individual is available for capture at hospital
+#' 
+#' @return A `result` list containing:
+#' Function input parameters,
+#' `h` capture history with 0s and 1s indicators,
+#' `h_2` capture history with 0s, 1s and 2s indicators,
+#' `a` lab test availability history,
+#' `b` hospital availability history,
+#' `obs_a` observed lab test availability history,
+#' `obs_b` observed hospital availability history,
+#' augmented versions of the histories.
 
 make_history <- function(N, M, k, pa, pb, theta1, theta2) {
-  set.seed(5)
+  set.seed(5) # for random number generation
   
   a <- matrix(NA, nrow = N, ncol = k)
   b <- matrix(NA, nrow = N, ncol = k)
-  h <- matrix(0, nrow = N, ncol = k)
-  h_2 <- matrix(0, nrow = N, ncol = k) # contains 2s for recaptures
+  h <- matrix(0, nrow = N, ncol = k) # contains 0s 1s indicating captures
+  h_2 <- matrix(0, nrow = N, ncol = k) # contains 0s 1s 2s indicating specific capture occasions
   
-  #' first, simulate latent a and b and use them to create h and h_2
+  #' first, simulate latent `a` and `b` and use them to create `h` and `h_2`
   for (i in 1:N) {
     for (j in 1:k) {
       if (j==1) {
-        # must populate b first: if available for both lab test and hospital in the same time period then only count b
+        #' must populate `b` first: if available for both lab test and hospital in the same time period then only count `b` (Assumption) 
         b[i,j] <- 0
         a[i,j] <- rbern(1, theta1)
       } else {
@@ -31,8 +41,8 @@ make_history <- function(N, M, k, pa, pb, theta1, theta2) {
         a[i,j] <- rbern(1, prod(1-a[i,], na.rm=T) * prod(1-b[i,], na.rm=T) * theta1)
       }
       
-      # for h_2, indicate capture by 1 and recapture by 2 
-      # in h, both are 1s
+      #' for `h_2`, indicate captures by `1`s and recaptures by `2`s 
+      #' in `h`, both are `1`s
       if (rbern(1, a[i,j] * pa) == 1) { 
         h_2[i,j] <- 1 
         h[i,j] <- 1
@@ -44,9 +54,9 @@ make_history <- function(N, M, k, pa, pb, theta1, theta2) {
     }
   }
   
-  #' second, use h_2 to reconstruct certain/observed availability histories obs_a and obs_b as completely as possible 
-  #' a 1 in position #2 in obs_a indicates that a person is definitely around for lab testing during time period #2
-  #' see `populate_row_obs_ab()` for examples
+  #' second, use `h_2` to reconstruct certain/observed availability histories `obs_a` and `obs_b` as completely as possible 
+  #' for example, a `1` in position #2 in `obs_a` indicates that a person is definitely around for lab testing during time period #2
+  #' see `populate_row_obs_ab()` for more examples
   obs_ab <- h_2 %>% apply(1, populate_row_obs_ab) %>% t()
   obs_a <- obs_ab[,1:k]
   obs_b <- obs_ab[,(k+1):(2*k)]
--- R/populate_row_obs_ab.R
+++ R/populate_row_obs_ab.R
@@ -6,9 +6,9 @@
 
 populate_row_obs_ab <- function(h_row) {
   k <- length(h_row)
-  #' for h_2 that looks like (a 1 always precedes a 2)
+  #' for `h_2` that looks like (a `1` always precedes a `2`)
   #' [1,]    0 1 2 0 0
-  #' make other positions 0s for obs_a and make other positions 0s obs_b
+  #' make other positions `0`s for `obs_a` and make other positions `0`s `obs_b`
   #' obs_a = 0 1 0 0 0
   #' obs_b = 0 0 1 0 0
   if (sum(h_row) == 3) {
@@ -21,9 +21,9 @@ populate_row_obs_ab <- function(h_row) {
     obs_b_row <- rep(0, k)
     obs_b_row[h_row_2_idx] <- 1
   }
-  #' for h_2 that looks like
+  #' for `h_2` that looks like
   #' [2,]    0 0 1 0 0
-  #' make other positions 0s for obs_a and make prior positions 0s for obs_b
+  #' make other positions `0`s for `obs_a` and make prior positions `0`s for `obs_b`
   #' obs_a = 0 0 1  0  0
   #' obs_b = 0 0 0 NA NA
   else if (sum(h_row) == 1) {
@@ -35,9 +35,9 @@ populate_row_obs_ab <- function(h_row) {
     obs_b_row <- rep(NA, k)
     obs_b_row[1:h_row_1_idx] <- 0
   }
-  #' for h_2 that looks like
+  #' for `h_2` that looks like
   #' [3,]    0    0    2    0    0
-  #' make subsequent positions 0s for obs_a and make other positions 0s for obs_b
+  #' make subsequent positions `0`s for `obs_a` and make other positions `0`s for `obs_b`
   #' obs_a = NA NA 0 0 0
   #' obs_b =  0  0 1 0 0
   else if (sum(h_row) == 2) {
